8cdb375c35aaa4a6da7b2cb41d398eb795703ef9
Musti Hussaini <musti@outlook.de>
2022-02-22 09:22:26 +0100

    isntalled pwsh for this user

--- pkg/windows.ps1
+++ pkg/windows.ps1
@@ -1,42 +1,45 @@
 param([switch]$Elevated)
 function Test-Admin {
-$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
 
-$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
-if ((Test-Admin) -eq $false)
-{
-if ($elevated) {
-# tried to elevate, did not work, aborting
-} else {
-Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-}
-Exit
+if ((Test-Admin) -eq $false) {
+  if ($elevated) {
+    # tried to elevate, did not work, aborting
+  }
+  else {
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+  }
+  Exit
 }
 
-if (-not(Get-Command "choco -v" -errorAction SilentlyContinue))
-{
+if (-not(Get-Command "choco -v" -errorAction SilentlyContinue)) {
   Write-Output "Chololatey does not exist! Installing..."
   # install chocolatey
-  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-} else {
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+else {
   Write-Output "Chocolatey is already installed. Continueing..."
 }
 
-if (-not(Get-Command "vboxmanage --version" -errorAction SilentlyContinue))
-{
+if (-not(Get-Command "vboxmanage --version" -errorAction SilentlyContinue)) {
   Write-Output "Virtual-Box does not exist! Installing..."
   # install virtualbox
   choco install virtualbox -y
-} else {
+}
+else {
   Write-Output "Virtual-Box is already installed. Continueing..."
 }
 
 
-if (-not(Get-Command "vagrant -v" -errorAction SilentlyContinue)) 
-{
+if (-not(Get-Command "vagrant -v" -errorAction SilentlyContinue)) {
   Write-Output "Vagrant does not exist! Installing..."
   choco install vagrant
-} else {
+}
+else {
   Write-Output "Vagrant is already isntalled. Continueing..."
 }
+
+vboxmanage hostonlyif create;
+vboxmanage hostonlyif ipconfig "VirtualBox Host-Only Ethernet Adapter" --ip 192.168.56.1;
